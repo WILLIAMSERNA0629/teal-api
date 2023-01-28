6edb245bc2fc8f33e9f5c044bf545d3f66b5fb6d
william serna <wserna880@gmail.com>
2023-01-28 17:45:39 -0500

    add cors config

--- app.js
+++ app.js
@@ -37,5 +37,13 @@ app.use('/users', usersRouters);
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 module.exports = app;

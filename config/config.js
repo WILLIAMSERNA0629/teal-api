7d8826abb6916d3d3b386dc2075132954d494610
william serna <wserna880@gmail.com>
2023-01-27 19:42:02 -0500

    change config

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 {
-  "development": {
+  "development"= {
     "username": "root",
     "password": "root",
     "database": "database_sequelize",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "test": {
-    "username": "process.env.DB.USER",
+  "test"= {
+    "username": process.env.DB.USER,
     "password": null,
     "database": "database_sequelize",
-    "host": "process.env.DB.HOST",
+    "host": process.env.DB.HOST,
     "dialect": "mysql"
   },
-  "production": {
-    "username": "process.env.DB.USER",
-    "password": "process.env.DB.PASSWORD",
+  "production"= {
+    "username": process.env.DB.USER,
+    "password": process.env.DB.PASSWORD,
     "database": "database_sequelize",
-    "host": "process.env.DB.HOST"  ,
+    "host": process.env.DB.HOST ,
     "dialect": "mysql"
   }
 }
